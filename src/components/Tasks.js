db40d786438020478c8d4012b9433a8b6380dcd0
Bart <bartlomiej.ra@gmail.com>
2021-02-02 22:01:27 +0100

    react first steps

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,30 +1,13 @@
+import Task from './Task'
 
-import {useState} from 'react'
-
-
- 
-  const Tasks = () => {
-    const [tasks, setTasks] = useState( [
-      {
-        "id": 1,
-        "text": "Doctors Appointment",
-        "day": "Feb 5th at 2:30pm",
-        "reminder": true
-      },
-      {
-        "id": 2,
-        "text": "Meeting at School",
-        "day": "Feb 6th at 1:30pm",
-        "reminder": true
-      }
-    ])
-    return (
-        <>
-            {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>  ))}
-        </>
-    )
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+  return (
+    <>
+      {tasks.map((task, index) => (
+        <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
+      ))}
+    </>
+  )
 }
-
-
 
 export default Tasks
